87d908d8474054c45790c5bb22d83aa698ad297b
agayevkenn <agaeyevk580@gmail.com>
2025-05-20 22:45:53 +0400

    base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  page: "/film",
+  base: "/film",
   server: {
     open: true,
   }
