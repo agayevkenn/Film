fc6943b5eef8b011a422d095eb0b31ca081c0aab
agayevkenn <agaeyevk580@gmail.com>
2025-05-20 22:11:19 +0400

    last update

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import { HashRouter } from "react-router-dom";
 import React from "react";
 import "./index.css";
